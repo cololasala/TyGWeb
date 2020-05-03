6c9abd06d5d8117f94ca9d87d4df30f0dcab7dc1
cololasala <luciano.lasala12@hotmail.com>
2020-05-02 22:30:11 -0300

    work updated

--- ejercicio.js
+++ ejercicio.js
@@ -2,17 +2,34 @@ var app = new Vue({
 	el:"#app",
 	data: {
 		message: 'hello world',
+		showElements: false,
+		showElementsTwo: false,
 		elements: null,
+		elementsTwo: null,
+		phrasesArray:[{phrase:'Hola a todos :D', style:1}, {phrase:'Pagina unica en el mundo', style:2} ,{phrase:'Esto es muy divertido!!', style:3}],
 	},
 
 	methods: {
+
 		api1() {
-			alert('hello!');
+			this.showElementsTwo = false;
+			this.showElements = true;
+		},
+
+		api2() {
+			this.showElements = false;
+			this.showElementsTwo = true;
 		}
 
 	},
+
+	created() {
+		this.showPhrase = this.phrasesArray[Math.floor(Math.random() * 3)]; // toma valores entre 0 y 1, luego multiplica por 3 y luego toma la parte entera 
+	},
+
 	mounted() {
 		axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-			.then(response => this.elements = response.data.bpi)
+			.then(response => this.elements = response.data.bpi);
+
 	}
 })
